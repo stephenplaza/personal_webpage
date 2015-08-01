21dfbd360f60afe819f2b2a95b52cae0a81ba763
Stephen Plaza <plazas@janelia.hhmi.org>
2015-08-01 11:07:49 -0400

    twitter reload hack

--- src/js/components/Twitter.react.js
+++ src/js/components/Twitter.react.js
@@ -3,6 +3,23 @@
 var React = require('react');
 
 var Twitter = React.createClass({
+    componentDidUpdate: function () {
+        alert("blah");
+
+    },
+    loadTwitter: function () {
+        var d = document;
+        var s = "script";
+        var id = "twitter"; 
+            
+        var js, fjs=d.getElementsByTagName(s)[0],
+            p=/^http:/.test(d.location)?'http':'https';
+        
+        //if(!d.getElementById(id)) {
+            js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);
+        //}
+
+    },
     componentDidMount: function () {
         var d = document;
         var s = "script";
@@ -16,6 +33,11 @@ var Twitter = React.createClass({
         }
     },
     render: function () {
+        var twitterel = document.getElementById("twitter");
+        if(twitterel) {
+            this.loadTwitter();
+        }
+        
         return (
                 <a className="twitter-timeline" data-dnt="true" href="https://twitter.com/stephenplaza" data-widget-id="625856147061370880">
                 Tweets by @stephenplaza
